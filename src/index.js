2122f9c4dcf2ac5deb68bc8a3e4f06b798b7ef6a
hyo <kdhyo98@gmail.com>
2020-09-02 00:02:21 +0900

    Subscription test add

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import Query from "./resolvers/Query";
 import Mutation from "./resolvers/Mutation";
 import User from "./resolvers/User";
 import Link from "./resolvers/Link";
+import Subscription from "./resolvers/Subscription";
+import Vote from "./resolvers/Vote";
 
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import { PrismaClient } from "@prisma/client";
 
+//prisma 클라이언트 연결용
 const prisma = new PrismaClient();
+
+//실시간 채팅에 대해서 배워봅시당.
+const pubsub = new PubSub();
 
 const resolvers = {
   Query,
   Mutation,
   User,
   Link,
+  Subscription,
+  Vote,
 };
 
 const server = new GraphQLServer({
@@ -22,6 +30,7 @@ const server = new GraphQLServer({
     return {
       ...request,
       prisma,
+      pubsub,
     };
   },
 });
--- src/resolvers/Link.js
+++ src/resolvers/Link.js
@@ -8,6 +8,11 @@ function postedBy(parent, args, context) {
   return context.prisma.link.findOne({ where: { id: parent.id } }).postedBy();
 }
 
+function votes(parent, args, context) {
+  return context.prisma.link.findOne({ where: { id: parent.id } });
+}
+
 module.exports = {
   postedBy,
+  votes,
 };
